839faf7891ed7da4010cea53329edd2bd3a12461
abiatarfestus <40578707+abiatarfestus@users.noreply.github.com>
2022-06-05 20:46:58 +0200

    Fixed IndexEror

--- worker_thread.py
+++ worker_thread.py
@@ -1,10 +1,10 @@
 import cv2
 import winsound
-import numpy as np
-from datetime import datetime
-from popups import display_message
+# import numpy as np
+# from datetime import datetime
+# from popups import display_message
 from pyzbar.pyzbar import decode, ZBarSymbol
-from PyQt5.QtCore import QObject, pyqtSignal, QTimer
+from PyQt5.QtCore import QObject, pyqtSignal
 from PyQt5.QtGui import QImage, QPixmap
 
 
@@ -21,7 +21,7 @@ class Worker(QObject):
         self.id_numbers = set()
 
     def run(self):
-        self.capture = cv2.VideoCapture(0)
+        self.capture = cv2.VideoCapture(0, apiPreference=cv2.CAP_ANY, params=[cv2.CAP_PROP_FRAME_WIDTH, 1280, cv2.CAP_PROP_FRAME_HEIGHT, 720])
         while self.camera_on:
             try:
                 ret, frame = self.capture.read()
@@ -35,14 +35,17 @@ class Worker(QObject):
                     app_number = qrc_data[1]
                     name = qrc_data[2]
                     if len(id_number)==11:
-                        # print(id_number, app_number, name, len(self.id_numbers))
                         self.id_numbers.add(id_number)
                         self.id_scanned.emit([id_number, app_number, name, len(self.id_numbers)])
             except Exception as e:
                 self.str_message.emit(repr(e))
                 self.thread_active = False
                 break
-        # cv2.destroyAllWindows()\
+        # cv2.destroyAllWindows()
+        width = self.capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+        height = self.capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        print(f"{width}x{height}")
+        print(self.capture.get(cv2.CAP_PROP_FPS))
         self.video_ended.emit("Waiting for video feed")
         self.finished.emit(self.id_numbers)
         self.capture.release()
@@ -58,19 +61,14 @@ class Worker(QObject):
 
             for obj in qrcode:
                 qrc_data = obj.data.decode("utf-8")
-                # print(f"FULL_DATA: {qrc_data}")
                 qrc_data_list = qrc_data.split(";")
-                # print(f"FULL_DATA_LIST: {qrc_data_list}")
                 id_number = qrc_data_list[0]
                 app_number = qrc_data_list[-1]
                 name = f"{qrc_data_list[1]} {qrc_data_list[2]}"
                 winsound.Beep(440, 500)
+        except IndexError:
+                pass
         except Exception as e:
             self.str_message.emit(repr(e))
             self.thread_active = False
         return [id_number, app_number, name]
-        
-    
-    # def stop_video(self):
-    #     self.thread_active = False
-        # self.finished.emit()
